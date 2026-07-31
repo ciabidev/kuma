4e87e612ee71a2de00fb4d97173206577e41d3e2
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2026-07-31 15:37:23 -0400

    feat: added some helpful modules and reworked subcommand laoder to use module

--- commands/moderation/main.js
+++ commands/moderation/main.js
@@ -1,33 +1,8 @@
-const {
-  SlashCommandBuilder,
-  PermissionsBitField,
-  MessageFlags,
-  Collection,
-} = require("discord.js");
+const registerSubcommandFolder = require("#modules/subcommandFolder");
 
-const fs = require("node:fs");
-
-module.exports = {
-  data: (() => {
-    const builder = new SlashCommandBuilder()
-      .setName("moderation")
-      .setDescription("Moderation commands")
-      .setDMPermission(false);
-
-    const dir = __dirname; // commands/moderation
-    const files = fs.readdirSync(dir).filter((file) => file.endsWith(".js") && file !== "main.js");
-
-    for (const file of files) {
-      const sub = require(`#commands/moderation/${file.slice(0, -3)}`);
-      builder.addSubcommand(() => sub.data);
-    }
-
-    return builder;
-  })(),
-
-  async execute(interaction) {
-    const name = interaction.options.getSubcommand(true);
-    const handler = require(`#commands/moderation/${name}`);
-    return handler.execute(interaction);
-  },
-};
+module.exports = registerSubcommandFolder({
+  name: "moderation",
+  description: "Moderation commands",
+  dirname: __dirname,
+  configure: (builder) => builder.setDMPermission(false),
+});
